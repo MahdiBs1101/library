72f835cd2e25c20517c54cbf19c8f652e4c4ae31
Mahdi Bastani <mahdibastani.cpp@gmail.com>
2026-06-12 00:27:03 +0330

    Add Main class

---
+++ src/main/library/main/Main.java
@@ -0,0 +1,11 @@
+package main.library.main;
+
+import main.library.ui.MainFrame;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new MainFrame();
+    }
+}
